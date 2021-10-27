74ee6f6d6108c3c50099926d18c7dc1125deb4e0
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2021-10-26 21:43:40 -0400

    s

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import Education from "./pages/education";
 import Home from "./pages/Home";
 import Resume from "./pages/Resume";
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <NavBar />
       <Switch>
-        <Route path="https://arzoid29.github.io/" exact={true} component={Home}></Route>
-        <Route path="https://arzoid29.github.io/myPort/resume" exact={true} component={Resume}></Route>
-        <Route path="https://arzoid29.github.io/myPort/education" exact={true} component={Education}></Route>
+        <Route path="*/" exact={true} component={Home}></Route>
+        <Route path="*/resume" exact={true} component={Resume}></Route>
+        <Route path="*/education" exact={true} component={Education}></Route>
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
